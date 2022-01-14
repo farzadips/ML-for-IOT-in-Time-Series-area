b62354f9f48759f05148813223b7de60b9f2e1af
Farzad ips <imanpourfarzad@gmail.com>
2022-01-14 18:22:23 +0100

    Lab5/Ex1,Ex2,Ex3,Ex4 done

--- .history/Lab5/ex2/twooperands_client_20220114153825.py
+++ .history/Lab5/ex2/twooperands_client_20220114153825.py
@@ -3,9 +3,9 @@ import requests
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-command', nargs=1, type=str)
-parser.add_argument('-op1', nargs=1, type=float)
-parser.add_argument('-op2', nargs=1, type=float)
+parser.add_argument('command', nargs=1, type=str)
+parser.add_argument('op1', nargs=1, type=float)
+parser.add_argument('op2', nargs=1, type=float)
 args = parser.parse_args()
 
 
--- .history/Lab5/ex3/multioperands_client_20220114171622.py
+++ .history/Lab5/ex3/multioperands_client_20220114171622.py
@@ -4,26 +4,25 @@ import requests
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-command', nargs=1, type=str)
-parser.add_argument('-op1', nargs=1, type=float)
-parser.add_argument('-op2', nargs=1, type=float)
+parser.add_argument('-operands', nargs='+', type=float)
 args = parser.parse_args()
 
 
 command = args.command[0]
-op1 = args.op1[0]
-op2 = args.op2[0]
+operands = args.operands
 
-#this line didn't work
-#url = 'http://0.0.0.0:8080/{}?op1={}&op2={}'.format(command, op1, op2)
+
 url = 'http://localhost:8080/{}?op1={}&op2={}'.format(command, op1, op2)
 
-r = requests.get(url)
+body = {'command': command, 'operands': operands}
+
+r = requests.put(url, json=body)
 
 commands = {'add': '+', 'sub': '-', 'mul': '*', 'div': '/'}
 
 if r.status_code == 200:
     body = r.json()
-    print(body['op1'], commands[body['command']], body['op2'], '=',
-            body['result'])
+    string = commands[body['command']].join(map(str, operands))
+    print(string, '=', body['result'])
 else:
     print('Error:', r.status_code)
